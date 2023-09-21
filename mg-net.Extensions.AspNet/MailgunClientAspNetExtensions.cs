5294e30153951d4d398eae6e4c0dbdf9b2536ba3
p-kaczynski <p.a.kaczynski@gmail.com>
2023-09-21 19:58:56 +0100

    Update MailgunClientAspNetExtensions.cs
    Use AddOptions instead of Configure

--- mg-net.Extensions.AspNet/MailgunClientAspNetExtensions.cs
+++ mg-net.Extensions.AspNet/MailgunClientAspNetExtensions.cs
@@ -1,22 +1,23 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace mg_net.Extensions.AspNet;
 
 public static class MailgunClientAspNetExtensions
 {
-    public static void AddMailgunClient(this IServiceCollection services, Action<MailgunClientOptions> optionAction,
+    public static void AddMailgunClient(this IServiceCollection services, Action<OptionsBuilder<MailgunClientOptions>> optionAction,
         Action<IServiceProvider, HttpClient> clientAction)
     {
-        services.Configure(optionAction);
+        optionAction.Invoke(services.AddOptions<MailgunClientOptions>());
         services.AddHttpClient<MailgunClient>(clientAction);
         services.AddScoped<MailgunClient>();
     }
-    
-    public static void AddMailgunClient(this IServiceCollection services, Action<MailgunClientOptions> optionAction,
+
+    public static void AddMailgunClient(this IServiceCollection services, Action<OptionsBuilder<MailgunClientOptions>> optionAction,
         Uri mailgunBaseUri)
         => AddMailgunClient(services, optionAction, (_, client) => { client.BaseAddress = mailgunBaseUri; });
 
-    public static void AddMailgunClient(this IServiceCollection services, Action<MailgunClientOptions> optionAction,
+    public static void AddMailgunClient(this IServiceCollection services, Action<OptionsBuilder<MailgunClientOptions>> optionAction,
         string mailgunBaseUri = "https://api.mailgun.net/")
         => AddMailgunClient(services, optionAction, new Uri(mailgunBaseUri, UriKind.Absolute));
 }
